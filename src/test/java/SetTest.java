645d8b29f4f0f4e27d10fc2d0f6d8b6250f1a1c2
장준희 <junheejang@wemakeprice.com>
2021-04-23 15:20:43 +0900

    feat: set size 테스트 추가

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set 크기 테스트")
+    void setSizeTest() {
+        // then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
